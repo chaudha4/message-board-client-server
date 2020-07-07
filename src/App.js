6a927e49e216d6fe738b1e92e0b3536d763b0123
Abhishek Chaudhary <chaudha4@hotmail.com>
2020-07-07 11:31:00 -0400

    Add react Board and Threads

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState, useRef, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [query, setQuery] = useState('');
+
+  const [boards, setBoards] = useState([]);
+
+  const getBoard = async (e) => {
+
+    e.preventDefault();
+
+    // Assumption is that the server is ruuing at the same domain as client.
+    //const url = window.location.href + `b/${query}/`;
+
+    const url = `https://chaudha4-mesgboard-mongo.glitch.me/api/threads/${query}/`
+    
+    console.log("Accessing URL %s", url);
+
+    try {
+      const res = await fetch(url);
+      console.log(res);
+      const data = await res.json();
+      console.log(data);
+      setBoards(data);
+    } catch (err) {
+      console.error(err);
+    }
+  }  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Message Board</h1>
+      {/* Refer to https://scrimba.com/p/pZaznUL/cE9N3nsw */}
+
+      <form className="form" onSubmit={getBoard}>
+        {/* htmlFor attribute specifies which form element a label is bound to. */}
+  {/*<label className="label" htmlFor="query">board</label>*/}
+        <input className="input" type="text" name="query"
+          placeholder="board name"
+          value={query} onChange={(e) => setQuery(e.target.value)}
+        />    
+        <button className="button" type="submit">Get</button>
+      </form>
+
+      <div className="card-list">
+        {boards.map(b => (
+          <div className="card" key={b._id}>
+            <h2 className="card--title">
+              {b.board} - thread "{b.text}"
+            </h2>
+            <p><b>Created On: </b>{b.created_on}</p>
+            <p><b>Last Updated: </b>{b.bumped_on}</p>
+            <p><b>Id: </b>{b.thread_id}</p>
+            <p><b>Replies: </b>{b.reply.length}</p>
+            {b.reply.map(reply => (
+              <p key={reply._id}>{reply.reply}</p>
+            ))}
+          </div>
+        ))}
+      </div>
+
     </div>
   );
 }
